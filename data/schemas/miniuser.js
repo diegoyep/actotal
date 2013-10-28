668d3a975e0785a7a36e7d1b0f76a917fb83077f
Diego Yepes <diego.yepes@zefira.pe>
2013-10-28 01:20:15 -0500

    Fixed user creation bug

--- data/schemas/miniuser.js
+++ data/schemas/miniuser.js
@@ -1,12 +1,10 @@
 var mongoose = require('mongoose');
-var shortId = require('shortid');
 
-shortId.seed(2930923);
 
 var MiniUseSchema = new mongoose.Schema({
 	email: {type: String, unique: true},
 	invites: {type: Array, default:[]},
-	_id: {type: String, unique: true, default: shortId.generate() }
-})
+	_id: {type: String, unique: true}
+});
 
 module.exports = MiniUseSchema;
