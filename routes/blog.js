ab7e884fe25ab7b678c41bb205e3acdcdbd65794
Diego Yepes <diego.yepes@zefira.pe>
2013-11-02 13:56:18 -0500

    blog complete

--- routes/blog.js
+++ routes/blog.js
@@ -4,8 +4,9 @@ var Post = require('../data/models/Post');
 
 module.exports = function(app){
 	app.get('/blog', function(req, res, next) {
-	var q = Post.find({}).sort('date', -1).limit(10);
+	var q = Post.find({}).limit(10);
 	q.execFind(function(err, posts) {
+		console.log(posts)
 		res.render('blog', {
 			title: 'Zefira | Blog', 
 			posts: posts
@@ -14,8 +15,8 @@ module.exports = function(app){
 		
 	}),
 
-	app.get('/blog/:slug', function(req, res, next){
-		Posts.findOne({slug : req.params.slug}, function(err, post){
+	app.get('/blog/show/:slug', function(req, res, next){
+		Post.findOne({slug : req.params.slug}, function(err, post){
 			res.render('post', {
 				title: 'Zefira | Blog',
 				post : post
